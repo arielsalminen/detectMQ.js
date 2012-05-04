3c646660b034de106b296dbac8c2f1d4eea13dfc
Viljami S <viljami.salminen@gmail.com>
2012-05-04 19:29:44 +0300

    Remove unnecessary code

--- detectMQ.js
+++ detectMQ.js
@@ -11,21 +11,11 @@
 
   var getValues = function () {
       dmq_contentValue = win.getComputedStyle(doc.body, ":after").getPropertyValue("content");
-      dmq_size = dmq_contentValue.replace(/['"]/g, '');
+      dmq_size = dmq_contentValue.replace(/['"]/g, "");
       callback();
-    },
-
-    complete = false,
-
-    initDmq = function () {
-      if (complete) {
-        return;
-      }
-      complete = true;
-      getValues();
     };
 
-  win.addEventListener("load", initDmq, false);
+  win.addEventListener("load", getValues, false);
   if (live === true) {
     win.addEventListener("resize", function () {
       clearTimeout(dmq_timeout);
