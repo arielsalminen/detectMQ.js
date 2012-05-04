dfc0b8406231fb85b6d00e24db5560eadc03d677
Viljami S <viljami.salminen@gmail.com>
2012-05-04 16:38:16 +0300

    Pushing out version 0.1

--- detectMQ.js
+++ detectMQ.js
@@ -1,92 +1,36 @@
-/*jslint browser: true, sloppy: true, vars: true, plusplus: true, indent: 2 */
+/*! detectMQ.js v0.1 by @viljamis */
+(function (win) {
 
-// @misteroneill's Resizestop event
-// https://github.com/misteroneill/resize-stop
-(function (window, setTimeout, Date) {
+  var dmq_timeout;
 
-  var cache = [],
-    last = 0,
-    timer = 0,
-    threshold = 100;
+  var dmq         = win.detect_mq || {},
+    live          = dmq.live || true,
+    threshold     = dmq.threshold || 300,
+    doc           = win.document,
+    callback      = dmq.callback || {};
 
-  window.addEventListener('resize', function () {
-    last = Date.now();
-    timer = timer || setTimeout(checkTime, 10);
-  }, false);
-
-  window.resizeStop = {
-
-    setThreshold: function (ms) {
-      if (typeof ms === 'number' && ms > -1 && !isNaN(ms) && isFinite(ms)) {
-        threshold = ms;
-        return ms;
-      }
-      return false;
+  var getValues = function () {
+      dmq_contentValue = win.getComputedStyle(doc.body, ":after").getPropertyValue("content");
+      dmq_size = dmq_contentValue.replace(/['"]/g, '');
+      callback();
     },
 
-    bind: function (callback) {
-      if (typeof callback === 'function') {
-        cache.push(callback);
-        return cache.length - 1;
-      }
-      return false;
-    },
+    complete = false,
 
-    unbind: function (what) {
-      var i = (typeof what === 'number') ? what : cache.indexOf(what);
-      if (i > -1) {
-        cache.splice(what, 1);
+    initDmq = function () {
+      if (complete) {
+        return;
       }
-      return i > -1;
-    }
+      complete = true;
+      getValues();
+    };
 
-  };
-
-  function checkTime() {
-    var now = Date.now();
-    if (now - last < threshold) {
-      timer = setTimeout(checkTime, 10);
-    } else {
-      clearTimeout(timer);
-      timer = last = 0;
-      for (var i = 0, max = cache.length; i < max; i++) {
-        cache[i]();
-      }
-    }
+  win.addEventListener("load", initDmq, false);
+  if (live === true) {
+    win.addEventListener("resize", function () {
+      clearTimeout(dmq_timeout);
+      dmq_timeout = setTimeout(getValues, initDmq);
+    }, false);
   }
 
-})(window, setTimeout, Date);
-
-// Function which does something
-function doSomething() {
-
-  var getStyle = document.getElementById("test"),
-    contentValue = window.getComputedStyle(document.body, ":after").getPropertyValue("content"),
-    size = contentValue.replace(/['"]/g, ''),
-    element = document.getElementById("which");
-
-  //console.log(size);
-
-  if (size === "phone") { element.innerHTML = "phone"; }
-  if (size === "tablet") { element.innerHTML = "tablet"; }
-  if (size === "laptop") { element.innerHTML = "laptop"; }
-  if (size === "desktop") { element.innerHTML = "desktop"; }
-
-}
-
-// Logic to call the above function
-if (live === true) {
-  window.onload = function () {
-    doSomething();
-  };
-  window.onresize = function () {
-    doSomething();
-  };
-
-// If "live" is false, then trigger on resize stop
-} else {
-  window.onload = function () {
-    doSomething();
-    window.resizeStop.bind(doSomething);
-  };
-}
+}(this));
